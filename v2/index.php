a9ba8d1f339a6a1854978cf5939ef26cb591de72
Sebastian Orbom <zebban90a@gmail.com>
2021-05-06 12:34:56 +0200

    last push

--- v2/index.php
+++ v2/index.php
@@ -2,12 +2,18 @@
 <?php require_once('../functions.php'); ?>
 
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Referrer-Policy: no-referrer");
+header("Content-Type: application/json; charset=UTF-8");
 
-$show = $_GET['show'] ?? null;
-$category = $_GET['category'] ?? null;
+$show = $_GET['show'] ?? false;
+$category = $_GET['category'] ?? false;
 
 
 $result = $arr;
+$errors = array();
 
 try{
     if(isset($category)){
@@ -23,8 +29,12 @@ try{
     $json = json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     echo $json;
 } catch (Exception $e) {
-    $obj -> Error = $e->getMessage();
-    $json = json_encode($obj, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    //$obj -> Error = $e->getMessage();
+    array_push($errors, ['fel' => $e->getMessage()]);
+}
+
+if(!empty($errors)) {
+    $json = json_encode($errors, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     echo $json;
 }
 
